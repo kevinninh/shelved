424e2efccf337b31d12d16feff44c6c7e275f2c8
Kevin Ninh <kevinninh7@outlook.com>
2026-09-16 20:27:16 -0400

    adjustment

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!lqn4@_9t5_o6mjd5sl&%n^*fod5me&kqu8jb8xn@d=9hh+3e^'
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-local-development-only",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'games.apps.GamesConfig',
+    'gsmrd.sppd.qr.apps.QrConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
