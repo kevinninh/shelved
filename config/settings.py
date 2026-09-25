19196f53b7912be41c47f7c2b2b7d2727037d5f7
Kevin Ninh <kevinninh7@outlook.com>
2026-09-24 20:05:39 -0400

    che

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'games.apps.GamesConfig',
     'django.contrib.admin',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
